eae614fd8eb0db29f85364a55299832a70cb6e7a
imtia33 <axistro.freefire@gmail.com>
2026-05-06 21:29:25 +0600

    initial commit

---
+++ DrawSync/Models/User.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace DrawSync.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PasswordHash { get; set; } = null!;
+        public int RoleId { get; set; }
+        public Role Role { get; set; } = null!;
+        public DateTime CreatedAt { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
